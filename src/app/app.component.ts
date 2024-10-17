6c2ea08a57d57956bbb33f2424d7488193ee2888
davids <deivid.develop@gmail.com>
2024-10-17 11:32:44 +0200

    feat: Add ButtonModule and CommonModule imports to app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,22 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { ButtonModule } from 'primeng/button';
+import { ThemeService } from './shared/theme.service';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ButtonModule, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'static-template';
+
+  constructor(private themeService: ThemeService) {}
+
+  changeTheme(event: Event): void {
+    const theme = (event.target as HTMLSelectElement).value;
+    this.themeService.setTheme(theme);
+  }
 }
