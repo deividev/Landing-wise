32d6143c74f92c26073811225f50fff90f7592fd
davids <deivid.develop@gmail.com>
2024-10-17 12:57:47 +0200

    refactor: Update angular.json and styles.scss
    - Remove unused imports and update styles in angular.json and styles.scss
    - Add "src/assets" to the assets array in angular.json

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,27 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ButtonModule } from 'primeng/button';
+import { languages } from './datai18n';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ButtonModule],
+  imports: [RouterOutlet, ButtonModule, TranslateModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'static-template';
+
+  languages = languages;
+
+  constructor(public translationService: TranslateService) {
+    translationService.setDefaultLang('en');
+  }
+
+  changeLang(event: Event) {
+    const target = event.target as HTMLInputElement;
+    this.translationService.use(target.value);
+  }
 }
