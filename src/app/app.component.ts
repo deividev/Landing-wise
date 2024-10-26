6b3e64c56a07206e9f028d14711253bc966af386
davids <deivid.develop@gmail.com>
2024-10-26 11:23:22 +0200

    refactor: Update app component to use landing component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,27 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { ButtonModule } from 'primeng/button';
-import { languages } from './datai18n';
-import { TranslateModule, TranslateService } from '@ngx-translate/core';
-
+import { LandingComponent } from '../components/landing/landing.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ButtonModule, TranslateModule],
+  imports: [RouterOutlet, LandingComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'static-template';
-
-  languages = languages;
-
-  constructor(public translationService: TranslateService) {
-    translationService.setDefaultLang('es');
-  }
-
-  changeLang(event: Event): void {
-    const target = event.target as HTMLInputElement;
-    this.translationService.use(target.value);
-  }
 }
